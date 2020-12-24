d4e64be90aeefac0a6087c116b1bfff08c536190
Karthigai Selvan Murugesan <49098430+karthigaiselvanm@users.noreply.github.com>
2020-12-24 16:30:06 +0530

    Adding new project

--- Datawarehouse with AWS Redshift/sql_queries.py
+++ Datawarehouse with AWS Redshift/sql_queries.py
@@ -12,19 +12,18 @@ IAM_ROLE = config.get("IAM_ROLE","ARN")
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
-staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
-songplay_table_drop = "DROP TABLE IF EXISTS fact_songplay"
-user_table_drop = "DROP TABLE IF EXISTS dim_user"
-song_table_drop = "DROP TABLE IF EXISTS dim_song"
-artist_table_drop = "DROP TABLE IF EXISTS dim_artist"
-time_table_drop = "DROP TABLE IF EXISTS dim_time"
+stage_events_table_drop = "DROP TABLE IF EXISTS stage_events"
+stage_songs_table_drop = "DROP TABLE IF EXISTS stage_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay_ft"
+user_table_drop = "DROP TABLE IF EXISTS user_dim"
+song_table_drop = "DROP TABLE IF EXISTS song_dim"
+artist_table_drop = "DROP TABLE IF EXISTS artist_dim"
+time_table_drop = "DROP TABLE IF EXISTS time_dim"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
-CREATE TABLE IF NOT EXISTS staging_events
-(
+stage_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS stage_events(
 artist          VARCHAR,
 auth            VARCHAR, 
 firstName       VARCHAR,
@@ -46,8 +45,8 @@ userId          INTEGER
 );
 """)
 
-staging_songs_table_create = ("""
-CREATE TABLE IF NOT EXISTS staging_songs
+stage_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS stage_songs
 (
 song_id            VARCHAR,
 num_songs          INTEGER,
@@ -63,10 +62,10 @@ artist_location    VARCHAR
 """)
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS fact_songplay
+CREATE TABLE IF NOT EXISTS songplay_ft
 (
-songplay_id          INTEGER IDENTITY(0,1) PRIMARY KEY sortkey,
-start_time           TIMESTAMP,
+songplay_id          INTEGER IDENTITY(1,1) PRIMARY KEY distkey,
+start_time           TIMESTAMP sortkey,
 user_id              INTEGER,
 level                VARCHAR,
 song_id              VARCHAR,
@@ -78,29 +77,29 @@ user_agent           VARCHAR
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS dim_user
+CREATE TABLE IF NOT EXISTS user_dim
 (
-user_id INTEGER PRIMARY KEY distkey,
-first_name      VARCHAR,
-last_name       VARCHAR,
-gender          VARCHAR,
+user_id INTEGER PRIMARY KEY sortkey distkey,
+first_name      VARCHAR(30),
+last_name       VARCHAR(30),
+gender          CHAR(1),
 level           VARCHAR
 );
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS dim_song
+CREATE TABLE IF NOT EXISTS song_dim
 (
-song_id     VARCHAR PRIMARY KEY,
+song_id     VARCHAR PRIMARY KEY distkey,
 title       VARCHAR,
-artist_id   VARCHAR distkey,
+artist_id   VARCHAR sortkey,
 year        INTEGER,
 duration    FLOAT
 );
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS dim_artist
+CREATE TABLE IF NOT EXISTS artist_dim
 (
 artist_id          VARCHAR PRIMARY KEY distkey,
 name               VARCHAR,
@@ -111,7 +110,7 @@ longitude          FLOAT
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS dim_time
+CREATE TABLE IF NOT EXISTS time_dim
 (
 start_time    TIMESTAMP PRIMARY KEY sortkey distkey,
 hour          INTEGER,
@@ -119,14 +118,14 @@ day           INTEGER,
 week          INTEGER,
 month         INTEGER,
 year          INTEGER,
-weekday       INTEGER
+weekday       VARCHAR(9)
 );
 """)
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-    COPY staging_events FROM {}
+stage_events_copy = ("""
+    COPY stage_events FROM {}
     CREDENTIALS 'aws_iam_role={}'
     COMPUPDATE OFF region 'us-west-2'
     TIMEFORMAT as 'epochmillisecs'
@@ -134,8 +133,8 @@ staging_events_copy = ("""
     FORMAT AS JSON {};
 """).format(LOG_DATA, IAM_ROLE, LOG_PATH)
 
-staging_songs_copy = ("""
-    COPY staging_songs FROM {}
+stage_songs_copy = ("""
+    COPY stage_songs FROM {}
     CREDENTIALS 'aws_iam_role={}'
     COMPUPDATE OFF region 'us-west-2'
     FORMAT AS JSON 'auto' 
@@ -145,7 +144,7 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
-INSERT INTO fact_songplay(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+INSERT INTO songplay_ft(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
 SELECT DISTINCT to_timestamp(to_char(se.ts, '9999-99-99 99:99:99'),'YYYY-MM-DD HH24:MI:SS'),
                 se.userId as user_id,
                 se.level as level,
@@ -154,45 +153,45 @@ SELECT DISTINCT to_timestamp(to_char(se.ts, '9999-99-99 99:99:99'),'YYYY-MM-DD H
                 se.sessionId as session_id,
                 se.location as location,
                 se.userAgent as user_agent
-FROM staging_events se
-JOIN staging_songs ss ON se.song = ss.title AND se.artist = ss.artist_name;
+FROM stage_events se
+JOIN stage_songs ss ON se.song = ss.title AND se.artist = ss.artist_name;
 """)
 
 user_table_insert = ("""
-INSERT INTO dim_user(user_id, first_name, last_name, gender, level)
+INSERT INTO user_dim(user_id, first_name, last_name, gender, level)
 SELECT DISTINCT userId as user_id,
                 firstName as first_name,
                 lastName as last_name,
                 gender as gender,
                 level as level
-FROM staging_events
+FROM stage_events
 where userId IS NOT NULL;
 """)
 
 song_table_insert = ("""
-INSERT INTO dim_song(song_id, title, artist_id, year, duration)
+INSERT INTO song_dim(song_id, title, artist_id, year, duration)
 SELECT DISTINCT song_id as song_id,
                 title as title,
                 artist_id as artist_id,
                 year as year,
                 duration as duration
-FROM staging_songs
+FROM stage_songs
 WHERE song_id IS NOT NULL;
 """)
 
 artist_table_insert = ("""
-INSERT INTO dim_artist(artist_id, name, location, latitude, longitude)
+INSERT INTO artist_dim(artist_id, name, location, latitude, longitude)
 SELECT DISTINCT artist_id as artist_id,
                 artist_name as name,
                 artist_location as location,
                 artist_latitude as latitude,
                 artist_longitude as longitude
-FROM staging_songs
+FROM stage_songs
 where artist_id IS NOT NULL;
 """)
 
 time_table_insert = ("""
-INSERT INTO dim_time(start_time, hour, day, week, month, year, weekday)
+INSERT INTO time_dim(start_time, hour, day, week, month, year, weekday)
 SELECT distinct ts,
                 EXTRACT(hour from ts),
                 EXTRACT(day from ts),
@@ -200,13 +199,13 @@ SELECT distinct ts,
                 EXTRACT(month from ts),
                 EXTRACT(year from ts),
                 EXTRACT(weekday from ts)
-FROM staging_events
+FROM stage_events
 WHERE ts IS NOT NULL;
 """)
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
+create_table_queries = [stage_events_table_create, stage_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [stage_events_table_drop, stage_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [stage_events_copy, stage_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
